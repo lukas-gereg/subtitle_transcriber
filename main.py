1ddd934a109c86954d7d1d2f118d8264248fbf0e
Lukas Gereg <lukas.gereg@softacus.com>
2025-01-16 23:27:42 +0100

    testing if preprocessing and loading will work

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import io
+import numpy as np
+import torch
+import whisper
 import tempfile
 from pydub import AudioSegment
 from moviepy import VideoFileClip
 from pydub.silence import detect_nonsilent
 from pydub.playback import play
+from subprocess import CalledProcessError, run
+
 
 def extract_audio(movie_path: str, frame_rate: int = 16000):
     movie = VideoFileClip(movie_path)
@@ -28,10 +33,35 @@ if __name__ == "__main__":
     aud_buffer = extract_audio(video_path, frame_rate=rate)
     audio = AudioSegment.from_file(aud_buffer, format="wav")
 
-    non_silent_ranges = detect_nonsilent(audio, min_silence_len=500, silence_thresh=-40)
+    non_silent_ranges = detect_nonsilent(audio, min_silence_len=500, silence_thresh=-60)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = whisper.load_model("large", device=device)
 
     for idx, (start, end) in enumerate(non_silent_ranges):
-        print(idx)
+        if idx >= 1:
+            continue
+        start = start - 500 if start - 500 > 0 else start
+        end = end + 500 if (end + 500) < len(audio) else end
         audio_chunk = audio[start:end]
         chunk_io = io.BytesIO()
         audio_chunk.export(chunk_io, format="wav")
+        chunk_io.seek(0)
+        chunk_io.name = "temp.wav"
+        cmd = [
+            "ffmpeg",
+            "-nostdin",
+            "-threads", "0",
+            "-i", chunk_io,
+            "-f", "s16le",
+            "-ac", "1",
+            "-acodec", "pcm_s16le",
+            "-ar", str(16000),
+            "-"
+        ]
+        # fmt: on
+        try:
+            out = run(cmd, capture_output=True, check=True).stdout
+            print(np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0)
+        except CalledProcessError as e:
+            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
