b967e989216c8c188b16310ddbaa813066e36638
Lukas Gereg <lukas.gereg@softacus.com>
2025-02-02 23:15:18 +0100

    denoising rough shape done

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import torch
 import tempfile
 from pydub import AudioSegment
 from moviepy import VideoFileClip
+import torchaudio
 from transformers import pipeline
 from pydub.silence import detect_nonsilent
 from pydub.playback import play
+from demucs.pretrained import get_model
+from demucs.apply import apply_model
+import numpy as np
+from denoiser import pretrained
 
 
 def extract_audio(movie_path: str, frame_rate: int = 16000):
@@ -24,6 +29,48 @@ def extract_audio(movie_path: str, frame_rate: int = 16000):
 
     return buffer
 
+def denoise_audio(audio: AudioSegment, device: torch.device):
+    demucs_model = get_model("htdemucs_ft").to(device)
+    data, sr = audiosegment_to_tensor(audio)
+
+    with torch.no_grad():
+        sources = apply_model(demucs_model, data.unsqueeze(0), device=device)
+
+    speech_data = sources[3]
+    denoiser_model = pretrained.dns64().to(device)
+
+    with torch.no_grad():
+        denoised_vocals = denoiser_model(speech_data.unsqueeze(0)).squeeze(0)
+
+    audio_bytes = tensor_to_audio_bytes(denoised_vocals, sr)
+
+    audio_seg = AudioSegment.from_wav(audio_bytes)
+
+    return audio_seg
+
+def tensor_to_audio_bytes(audio: torch.Tensor, sr):
+    tensor = audio.cpu().numpy()
+    tensor = (tensor * np.iinfo(np.int16).max).astype(np.int16)
+
+    audio_bytes = io.BytesIO()
+    torchaudio.save(audio_bytes, torch.tensor(tensor), sr, format="wav")
+    audio_bytes.seek(0)
+
+    return audio_bytes
+
+def audiosegment_to_tensor(audio: AudioSegment, target_sr=44100):
+    samples = np.array(audio.get_array_of_samples(), dtype=np.float32)
+    samples /= np.iinfo(audio.array_type).max
+
+    num_channels = audio.channels
+    samples = samples.reshape((-1, num_channels)).T
+    wav = torch.tensor(samples)
+
+    if audio.frame_rate != target_sr:
+        wav = torchaudio.transforms.Resample(orig_freq=audio.frame_rate, new_freq=target_sr)(wav)
+
+    return wav, target_sr
+
 
 if __name__ == "__main__":
     audio_path = os.path.join(os.getcwd(), "movie_audio.wav")
@@ -32,31 +79,34 @@ if __name__ == "__main__":
     # aud_buffer = extract_audio(video_path, frame_rate=rate)
     audio = AudioSegment.from_file(audio_path, format="wav")
 
-    non_silent_ranges = detect_nonsilent(audio, min_silence_len=500, silence_thresh=-60)
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model="openai/whisper-large-v3",
-        chunk_length_s=30,
-        device=device,
-    )
+    audio = denoise_audio(audio, device)
+
+    non_silent_ranges = detect_nonsilent(audio, min_silence_len=500, silence_thresh=-40)
+
+    # pipe = pipeline(
+    #     "automatic-speech-recognition",
+    #     model="openai/whisper-large-v3",
+    #     chunk_length_s=30,
+    #     device=device,
+    # )
 
     for idx, (start, end) in enumerate(non_silent_ranges):
+        print(f"AUDIO INDEX {idx}")
         start = start - 500 if start - 500 > 0 else start
         end = end + 500 if (end + 500) < len(audio) else end
 
         audio_chunk = audio[start:end]
-        chunk_io = io.BytesIO()
-
-        audio_chunk.export(chunk_io, format="wav")
-        chunk_io.seek(0)
-
-        prediction = pipe(chunk_io, batch_size=8, return_timestamps=True, return_language=True)
+        # chunk_io = io.BytesIO()
+        #
+        # audio_chunk.export(chunk_io, format="wav")
+        # chunk_io.seek(0)
+        play(audio_chunk)
+        # prediction = pipe(chunk_io, batch_size=8, return_timestamps=True, return_language=True)
 
 from datetime import timedelta
 
-timeList = ['0:00:00', '0:00:15', '9:30:56', '21:00:00']
-total_time = sum(timedelta(hours=int(ms[0]), minutes=int(ms[1]), seconds=int(ms[2])) for t in timeList for ms in [t.split(":")],
-        timedelta())
+# timeList = ['0:00:00', '0:00:15', '9:30:56', '21:00:00']
+# total_time = sum(timedelta(hours=int(ms[0]), minutes=int(ms[1]), seconds=int(ms[2])) for t in timeList for ms in [t.split(":")],
+#         timedelta())
